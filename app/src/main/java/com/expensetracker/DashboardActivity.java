5e3962dc27d6f4b0a37546b813a1c52b721a4756
iamshri8 <shriram.narayanan@blueyonder.com>
2023-03-17 06:34:02 +0100

    UI: Add Transaction design page.

--- app/src/main/java/com/expensetracker/DashboardActivity.java
+++ app/src/main/java/com/expensetracker/DashboardActivity.java
@@ -4,15 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class DashboardActivity extends AppCompatActivity {
-    
+
+    private FloatingActionButton fabAddTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        goToAddTransactionPage();
+    }
+
+    private void goToAddTransactionPage() {
+        fabAddTransaction = findViewById(R.id.fab_add_transaction);
+
+        fabAddTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AddTransactionActivity.class));
+            }
+        });
     }
 
 }
